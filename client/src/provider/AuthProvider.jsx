44c9d0bea6005b9b3c4a48d85555446cd3d004ee
princemahmood117 <princemahmood117@gmail.com>
2024-12-20 21:01:01 +0600

    initialized firebase setup

--- client/src/provider/AuthProvider.jsx
+++ client/src/provider/AuthProvider.jsx
@@ -1,8 +1,11 @@
 import { createContext } from "react";
-
+import { app } from "../firebase/firebase.config";
+import { getAuth } from "firebase/auth";
 
 
 const  AuthContext = createContext(null)
+
+const auth = getAuth(app)
 
 const AuthProvider = ({children}) => {
     return (
